7bb07013858e66e3847d930fb2d49363b0a4303e
yulin <aaayulin@gmail.com>
2026-07-12 21:28:52 +0800

    feat: 重构模态框组件，优化显示和关闭逻辑

--- src/renderer/src/components/ModalShell.vue
+++ src/renderer/src/components/ModalShell.vue
@@ -1,19 +1,36 @@
 <script setup lang="ts">
 import { X } from '@lucide/vue'
+import { onMounted, ref } from 'vue'
+
 defineProps<{ title: string; width?: string; size?: 'default' | 'large' }>()
-defineEmits<{ close: [] }>()
+const emit = defineEmits<{ close: [] }>()
+
+const show = ref(false)
+
+onMounted(() => {
+  requestAnimationFrame(() => {
+    show.value = true
+  })
+})
+
+function handleClose(): void {
+  show.value = false
+  setTimeout(() => {
+    emit('close')
+  }, 180)
+}
 </script>
 
 <template>
-  <div class="modal-mask" @mousedown.self="$emit('close')">
+  <div class="modal-mask" :class="{ 'is-active': show }" @mousedown.self="handleClose">
     <div
       class="modal-card"
-      :class="{ 'is-large': size === 'large' }"
+      :class="{ 'is-large': size === 'large', 'is-active': show }"
       :style="size === 'large' ? undefined : { width: width || '480px' }"
     >
       <div class="modal-head">
         <div class="modal-title">{{ title }}</div>
-        <div class="icon-button" role="button" tabindex="0" @click="$emit('close')">
+        <div class="icon-button" role="button" tabindex="0" @click="handleClose">
           <X :size="18" />
         </div>
       </div>
